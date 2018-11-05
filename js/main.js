a71e12af1b7fc3ffad8131e59659269dd35b6075
Veronica Bautista <veronicabautista87@gmail.com>
2018-11-05 21:52:43 +0100

    add some styles and basic variables

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('>> Ready :)');
+var allPrices = [];
+var currency = '';
+var recipeName = '';
+var shippingPrice = 0;
+var priceToButton = 0;
+var itemsChecked = 0;
+var titleRecipe = document.querySelector('.tittle');
+var ingredients = document.querySelector('.ingredients__list');
+var shippingCost = document.querySelector('.shipping__cost');
+var totalPriceCont = document.querySelector('.total__price');
+var priceContainer = document.querySelector('.price__title');
+var subtotalContainer = document.querySelector('.subtotal');
+var subtotalContainerButton = document.querySelector('.buy__button');
+
+
+function addIngredients() {
+  fetch('https://raw.githubusercontent.com/Adalab/recipes-data/master/rissoto-setas.json')
+  .then(function(response){
+    return response.json();
+    });
+}
